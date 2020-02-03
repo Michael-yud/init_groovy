70181885be9c2a5b1dc03d44b5db3650070daf88
Ubuntu <ubuntu@ip-172-31-33-20.eu-central-1.compute.internal>
2020-02-03 14:09:04 +0000

    changed script

--- seeddsl.groovy
+++ seeddsl.groovy
@@ -1,3 +1,4 @@
+/*
 job('DSL-Tutorial-1-Test') {
     scm {
         git('git://github.com/quidryan/aws-sdk-test.git')
@@ -5,4 +6,100 @@ job('DSL-Tutorial-1-Test') {
     steps {
         maven('-e clean test')
     }
+}
+*/
+
+
+job('commit') {
+  description('Build and smoke test')
+
+  scm {
+    github('martinmosegaard/vigilant-sniffle')
+  }
+
+  steps {
+    gradle('jar')
+  }
+
+  // Artifactory configuration
+  configure {
+    it / buildWrappers / 'org.jfrog.hudson.gradle.ArtifactoryGradleConfigurator' {
+      deployMaven 'true'
+      deployIvy 'false'
+      deployBuildInfo 'true'
+      deployArtifacts 'true'
+      allowPromotionOfNonStagedBuilds 'true'
+      skipInjectInitScript 'true'
+
+      // Repository to resolve from
+      resolverDetails {
+        artifactoryName('artifactory')
+        artifactoryUrl('http://artifactory:8081/artifactory')
+        resolveReleaseRepository {
+          keyFromSelect('libs-release')
+        }
+      }
+      // Repository to deploy to.
+      details {
+        artifactoryName('artifactory')
+        artifactoryUrl('http://artifactory:8081/artifactory')
+        deployReleaseRepository {
+          keyFromSelect('libs-release-local')
+        }
+      }
+
+      artifactPattern '[organisation]/[module]/[revision]/[artifact]-[revision].$BUILD_NUMBER(-[classifier]).[ext]'
+    }
+  }
+
+  publishers {
+    downstream('test')
+  }
+}
+
+job('test') {
+  description('Acceptance test')
+
+  steps {
+    shell('''\
+    echo This is the test job.
+    '''.stripIndent())
+  }
+
+  publishers {
+    downstream('release')
+  }
+}
+
+job('release') {
+  description('Release the artifacts')
+
+  steps {
+    shell('''\
+    echo This is the release job.
+    '''.stripIndent())
+  }
+}
+
+buildPipelineView('Pipeline') {
+  title('vigilant-sniffle pipeline')
+  displayedBuilds(50)
+  selectedJob('commit')
+  alwaysAllowManualTrigger()
+  showPipelineParametersInHeaders()
+  showPipelineParameters()
+  showPipelineDefinitionHeader()
+  refreshFrequency(60)
+}
+
+// Jenkinsfile pipeline:
+pipelineJob('pipeline') {
+  definition {
+    cpsScm {
+      scm {
+        github('martinmosegaard/vigilant-sniffle')
+      }
+      scriptPath('Jenkinsfile')
+    }
+  }
 }
